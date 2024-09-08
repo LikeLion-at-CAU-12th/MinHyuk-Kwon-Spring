a9eee3beeec2a07d2ae30e0c793494e7cd1c3604
kmh0601 <pongkmh@naver.com>
2024-09-08 16:51:09 +0900

    [feat] health check api 추가

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class TestController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "hello spring! this is test1 by RequestMapping with RequestMethod.GET";
+    }
+
+    @GetMapping(value = "/test2")
+    public String test2() {
+        return "hello spring! this is test2 by GetMapping";
+    }
+}
