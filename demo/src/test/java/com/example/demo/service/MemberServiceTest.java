86b15bfa179b7860be3bb1a8fdf9c72fdca1096b
kmh0601 <pongkmh@naver.com>
2024-09-28 17:39:59 +0900

    [feat] spring4th standard

--- demo/src/test/java/com/example/demo/service/MemberServiceTest.java
+++ demo/src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -3,13 +3,18 @@ package com.example.demo.service;
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberJpaRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class MemberServiceTest {
@@ -41,5 +46,34 @@ public class MemberServiceTest {
     @Transactional
     public void testPrintMemberByPage() {
         memberService.printMemberByPage(0, 10); // 첫페이지의 10개 회원 정보 출력, size  값을 바꿔보면서 테스트!
+    }
+
+    @Test
+    @Transactional
+    public void tsetGetMembersByUsernameStartingWith() {
+        Page<Member> result = memberService.getMembersByUsernameStartingWith(0, 10, "user1");
+
+        // 'user1'로 시작하는지 확인
+        List<Member> members = result.getContent();
+        for (Member member : members) {
+            Assertions.assertTrue(member.getUsername().startsWith("user1"));
+        }
+
+        System.out.println("Member의 이름이 'user1'로 시작하는 수 : " + members.size());
+
+    }
+
+    @Test
+    @Transactional
+    public void testGetMembersByAgeGreaterThanEqual() {
+        Page<Member> result = memberService.getMembersByAgeGreaterThanEqual(0, 10, 20);
+
+        // 나이가 20 이상인지 확인
+        List<Member> members = result.getContent();
+        for (Member member : members) {
+            Assertions.assertTrue(member.getAge() >= 20);
+        }
+
+        System.out.println("20세 이상인 Member의 수 : " + members.size());
     }
 }
